f2ba38c2bfdadb7aeca5a188e912ba8e55f34f20
Dmitry <dsalahutdinov@gmail.com>
2021-06-21 11:21:22 +0500

    INitial commit

---
+++ lib/yabeda/delayed_job.rb
@@ -0,0 +1,8 @@
+require "yabeda/delayed_job/version"
+
+module Yabeda
+  module DelayedJob
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/yabeda/delayed_job/version.rb
@@ -0,0 +1,5 @@
+module Yabeda
+  module DelayedJob
+    VERSION = "0.1.0"
+  end
+end
